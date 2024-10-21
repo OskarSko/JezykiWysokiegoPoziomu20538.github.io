669b31c76abaa8e29c2bb5ddde618996a3775ca8
Oskar Skowroński <97964979+OskarSko@users.noreply.github.com>
2024-10-21 19:28:05 +0200

    Lab 4.1

--- SkowronskiOskarLabJS/index.js
+++ SkowronskiOskarLabJS/index.js
@@ -1,7 +1,28 @@
-import camelCase from 'camelcase';
+import http from 'http';
+import fs from 'fs';
 
-console.log(camelCase('nazwa-z-kreska'));
-console.log(camelCase('nazwa_z_podkresleniem'));
-console.log(camelCase('nazwa.z.kropka'));
-console.log(camelCase('nazwa z spacja'));
-console.log('Moduł zamienia -/_/./ / na camelCase');
+
+const server = http.createServer(function (req, res) {
+	getTitles(res);
+}).listen(8000, "127.0.0.1");
+function getTitles(res) {
+	fs.readFile('./titles.json', function (err, data) {
+		if (err) return hadError(err, res);
+		getTemplate(JSON.parse(data.toString()), res);
+	});
+}
+function getTemplate(titles, res) {
+	fs.readFile('./template.html', function (err, data) {
+		if (err) return hadError(err, res);
+		formatHtml(titles, data.toString(), res);
+	});
+}
+function formatHtml(titles, tmpl, res) {
+	let html = tmpl.replace('%', titles.join('</li><li>'));
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(html);
+}
+function hadError(err, res) {
+	console.error(err);
+	res.end('Error');
+}
